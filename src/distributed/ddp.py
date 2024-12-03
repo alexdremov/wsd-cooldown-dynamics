7e44892c2a6f9bf64a998f5678c2a60dc0266878
alexdremov <dremov.me@gmail.com>
2024-12-03 17:34:38 +0100

    remove weird acc logic

--- src/distributed/ddp.py
+++ src/distributed/ddp.py
@@ -27,9 +27,7 @@ class DataParallelDistributedBackend(DistributedBackend):
                 "{effective_batch_size} is not divisible "
                 "by the world size {world_size}."
             )
-        acc_steps_div = math.gcd(args.acc_steps, world_size)
-        args.acc_steps = args.acc_steps // acc_steps_div
-        args.batch_size = args.batch_size // (world_size // acc_steps_div)
+        args.batch_size = args.batch_size // world_size
         args.device = f"cuda:{self.local_rank}"
         args.seed = args.seed + self.local_rank
         args.data_seed = args.data_seed
